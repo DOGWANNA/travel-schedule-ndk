2c2d08841ef9787ba05c90d2c3fe4114fc8a6a80
나도관 <9401ndk@t-win.kr>
2026-08-24 15:41:55 +0900

    feat: MapAdapter 인터페이스 추가

---
+++ map-adapter.js
@@ -0,0 +1,14 @@
+class MapAdapter {
+  init(divId) { throw new Error('init() not implemented'); }
+  setCenter(lat, lng) { throw new Error('setCenter() not implemented'); }
+  setZoom(level) { throw new Error('setZoom() not implemented'); }
+  fitBounds(points) { throw new Error('fitBounds() not implemented'); }
+  addMarker(lat, lng, options) { throw new Error('addMarker() not implemented'); }
+  removeMarker(marker) { throw new Error('removeMarker() not implemented'); }
+  clearMarkers() { throw new Error('clearMarkers() not implemented'); }
+  drawPolyline(points, options) { throw new Error('drawPolyline() not implemented'); }
+  removePolyline(polyline) { throw new Error('removePolyline() not implemented'); }
+  clearPolyline() { throw new Error('clearPolyline() not implemented'); }
+  async fetchRoute(from, to) { throw new Error('fetchRoute() not implemented'); }
+  makeSpotIcon(type) { throw new Error('makeSpotIcon() not implemented'); }
+}
